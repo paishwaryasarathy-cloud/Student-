5c2604ffe1b0241e36a997b4497a7f670f1dcfd5
Aishwarya <p.aishwaryasarathy@gmail.com>
2026-07-25 10:49:54 +0530

    Add environment variable support

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StudentModule } from './student/student.module';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Student } from './student/student.model';
 @Module({
-  imports: [ SequelizeModule.forRoot({
-    dialect: 'mysql', 
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: '',
-    database: 'demo',
-    models: [Student],
-    autoLoadModels: true, 
-    synchronize: true, 
+  imports: [
+  ConfigModule.forRoot({
+    isGlobal: true,
   }),
+
+  SequelizeModule.forRoot({
+  dialect: 'mysql',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '3306'),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  models: [Student],
+  autoLoadModels: true,
+  synchronize: true,
+}),
   StudentModule],
   controllers: [AppController],
   providers: [AppService],
