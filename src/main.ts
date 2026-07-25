b6b2218b71ea314f02745517c68c944f867936e2
Aishwarya <p.aishwaryasarathy@gmail.com>
2026-07-25 13:17:15 +0530

    Fix CORS for Vercel frontend

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
     app.enableCors({
-    origin: 'http://localhost:3001',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
+  origin: [
+    'http://localhost:3001',
+    'https://student-frontend-six-lovat.vercel.app'
+  ],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+});
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
